a8e273df064fde8b6324a70b276a134de065cebc
mountaint0p <summit100@gmail.com>
2023-09-22 22:27:04 -0400

    Reuploaded lab 1

---
+++ timetabled/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Calendar from "./components/calendar";
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>timetable</h1>
+      <h2>Schedule</h2>
+      <Calendar />
+
+
+    </div>
+  )
+}
+
+export default App
